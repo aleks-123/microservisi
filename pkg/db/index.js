cda107e260761f0fa473d8c6b0b505063a3ae75f
aleks-123 <ace.ki@hotmail.com>
2023-11-08 23:06:11 +0100

    fff

--- pkg/db/index.js
+++ pkg/db/index.js
@@ -4,14 +4,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/../config/config.env` });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
 exports.init = async () => {
   try {
-    await mongoose.connect(DB, {
+    await mongoose.connect(process.env.DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
